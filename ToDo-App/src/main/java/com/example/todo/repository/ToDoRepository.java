0d3975a2b862eaa59d92bf8789b9231794ffa665
Kumari Amrita <imamritaa@gmail.com>
2023-09-28 10:24:21 +0530

    Initial Commit

---
+++ ToDo-App/src/main/java/com/example/todo/repository/ToDoRepository.java
@@ -0,0 +1,11 @@
+package com.example.todo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.todo.model.ToDo;
+
+@Repository
+public interface ToDoRepository extends JpaRepository<ToDo, Long> {
+
+}
